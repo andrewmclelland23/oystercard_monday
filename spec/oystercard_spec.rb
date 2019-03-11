3120075cca3afeb175c37a9aa1918ff321198310
Andrew McLelland <andrew.mclelland23@gmail.com>
2019-03-11 18:19:34 +0000

    Added tests for initializing oystercard with balance of 0 and top up method. Wrote code to pass tests

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,8 @@ describe Oystercard do
   it "should have a default balance of 0 when object is instantiated" do
     expect(subject.balance).to eq 0
   end
+  it "should be able to top up balance by passing in a value into the top_up method" do
+    subject.top_up(10)
+    expect(subject.balance).to eq 10
+  end
 end
